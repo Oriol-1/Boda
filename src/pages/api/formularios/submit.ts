74a150eee973106feb849d3b880ab67a56227e6e
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-01-31 21:31:47 +0100

    Si funciona

--- src/pages/api/formularios/submit.ts
+++ src/pages/api/formularios/submit.ts
@@ -5,34 +5,33 @@ import { connectToDatabase } from '../../../app/libs/mysql';
 // Definición de la estructura de los detalles de un hijo
 interface ChildDetail {
   name: string;
-  tipoMenu: string;
-  menuEspecial: boolean;
-  menuEspecialTipo: string;
-  menuEspecialTipoOtro: string;
+  menuType: string;
+  isSpecialMenu: boolean;
+  specialMenuType?: string;
 }
 
 // Definición de la estructura de los datos del invitado
 interface GuestData {
-  nombre: string;
-  menuPrincipalTipo: string;
-  menuPrincipalTipoEspecial?: string;
-  menuPrincipalTipoOtro?: string;
-  acompanante: boolean;
-  nombreAcompanante?: string;
-  menuAcompananteTipo?: string;
-  menuAcompananteTipoEspecial?: string;
-  menuAcompananteTipoOtro?: string;
-  hijos: boolean;
-  cantidadHijos?: number;
-  alergiaAlimentaria: boolean;
-  detallesAlergiaAlimentaria?: string;
-  telefonoContacto: string;
+  name: string;
+  menuType: string;
+  specialMenuType?: string;
+  customMenuType?: string;
+  hasCompanion: boolean;
+  companionName?: string;
+  companionMenuType?: string;
+  companionSpecialMenuType?: string;
+  companionCustomMenuType?: string;
+  hasChildren: boolean;
+  childrenCount?: number;
+  hasFoodAllergy: boolean;
+  allergyDetails?: string;
+  contactPhone: string;
   childrenDetails: ChildDetail[];
 }
 
 // Función principal que maneja las solicitudes HTTP a la ruta
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Datos recibidos:', req.body);  // Agregado para depuración
+  console.log('Datos recibidos:', req.body);  // Para depuración
 
   // Verificar que el método de la solicitud sea POST
   if (req.method !== 'POST') {
@@ -52,27 +51,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const guestData: GuestData = req.body;
 
     // Asegurarse de que los campos requeridos no sean undefined
-    if (!guestData.nombre || !guestData.menuPrincipalTipo || !guestData.acompanante || !guestData.hijos || !guestData.alergiaAlimentaria || !guestData.telefonoContacto) {
+    if (!guestData.name || !guestData.menuType || !guestData.hasCompanion || !guestData.hasChildren || !guestData.contactPhone) {
       res.status(400).json({ message: 'Faltan campos requeridos' });
       return;
     }
 
     // Preparar los valores para la inserción en la tabla 'invitados'
     const valuesInvitados = [
-      guestData.nombre,
-      guestData.menuPrincipalTipo,
-      guestData.menuPrincipalTipoEspecial || null,
-      guestData.menuPrincipalTipoOtro || null,
-      guestData.acompanante ? 1 : 0,
-      guestData.nombreAcompanante || null,
-      guestData.menuAcompananteTipo || null,
-      guestData.menuAcompananteTipoEspecial || null,
-      guestData.menuAcompananteTipoOtro || null,
-      guestData.hijos ? 1 : 0,
-      guestData.cantidadHijos || null,
-      guestData.alergiaAlimentaria ? 1 : 0,
-      guestData.detallesAlergiaAlimentaria || null,
-      guestData.telefonoContacto
+      guestData.name,
+      guestData.menuType,
+      guestData.specialMenuType || null,
+      guestData.customMenuType || null,
+      guestData.hasCompanion ? 1 : 0,
+      guestData.companionName || null,
+      guestData.companionMenuType || null,
+      guestData.companionSpecialMenuType || null,
+      guestData.companionCustomMenuType || null,
+      guestData.hasChildren ? 1 : 0,
+      guestData.childrenCount || null,
+      guestData.hasFoodAllergy ? 1 : 0,
+      guestData.allergyDetails || null,
+      guestData.contactPhone
     ];
 
     // Consulta SQL para insertar un nuevo invitado
@@ -92,18 +91,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const invitadoId = (resultInvitados as mysql.OkPacket).insertId;
 
     // Si el invitado tiene hijos, insertarlos en la tabla 'hijos'
-    if (guestData.hijos && guestData.childrenDetails && guestData.childrenDetails.length > 0) {
+    if (guestData.hasChildren && guestData.childrenDetails && guestData.childrenDetails.length > 0) {
       const queryHijos = `
-        INSERT INTO hijos (invitado_id, nombre, tipo_menu, menu_especial, menu_especial_tipo, menu_especial_tipo_otro)
+        INSERT INTO hijos (invitado_id, nombre, tipo_menu, menu_especial, menu_especial_tipo)
         VALUES ?
       `;
       const valuesHijos = guestData.childrenDetails.map((child: ChildDetail) => [
         invitadoId, 
         child.name, 
-        child.tipoMenu,
-        child.menuEspecial ? 1 : 0,
-        child.menuEspecialTipo || null,
-        child.menuEspecialTipoOtro || null
+        child.menuType,
+        child.isSpecialMenu ? 1 : 0,
+        child.specialMenuType || null
       ]);
       // Ejecución de la consulta para insertar hijos
       await db.query(queryHijos, [valuesHijos]);
