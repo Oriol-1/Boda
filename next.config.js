9da3e20eb2051aa5b65d5316a7d3026eaba7a746
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-02-24 22:25:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: '/.well-known/pki-validation/A02BE60F398BE4A8EB32A76CF0240B51.txt',
-        destination: '/A02BE60F398BE4A8EB32A76CF0240B51.txt',
-        permanent: true,
-      },
-    ]
-   },
-}
+module.exports = nextConfig
