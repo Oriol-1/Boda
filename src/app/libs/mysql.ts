e16396c736c8a97b983ef05e20e502cfb05db0cc
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-02-28 08:59:36 +0100

    Update mysql.ts

--- src/app/libs/mysql.ts
+++ src/app/libs/mysql.ts
@@ -2,11 +2,16 @@ import mysql from 'mysql2/promise';
 //conecta
 export async function connectToDatabase() {
   try {
-    const connection = await mysql.createConnection({
-      host: 'localhost',
+    const connection = await mysql.createConnection({      
+      //host: 'localhost',
+      //port: 3306,
+      //user: 'root',
+      //password: 'Basebodasabdra83',
+      //database: 'bodas'
+      host: 'instanciadbboda.c74u0c4wuzrm.eu-west-1.rds.amazonaws.com',
       port: 3306,
-      user: 'root',
-      password: 'Basebodasabdra83',
+      user: 'admin',
+      password: 'oriol1983',
       database: 'bodas'
     });
     console.log('Conexión exitosa a la base de datos');
