9a8dbb6ff5f6321ed84a3380da7078b76937d886
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-03-02 20:39:38 +0100

    cambio de nombre carpeta components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import Image from 'next/image'
 import Header from './ui/Header'
-import PrimeraParte from './componets/primeraParte/primeraParte'
+import PrimeraParte from './components/primeraParte/primeraParte'
 import FormularioPage from './../pages/formulario/index'
 import FechaBodaPage from './../pages/fechaBoda/fechaBoda'
 
--- src/pages/fechaBoda/fechaBoda.tsx
+++ src/pages/fechaBoda/fechaBoda.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import useCountdown from '../../app/libs/useCountdown';
-import Invitation from '../../app/componets/fecha/Fecha'; // Corregido a 'components'
+import Invitation from '../../app/components/fecha/Fecha'; // Corregido a 'components'
 import './stylesFech.css'
 
 const FechaBodaPage = () => {
