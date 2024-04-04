5c9378a010457b5c81d3a89e1dba1ecb54c23ea7
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-04-04 20:18:51 +0200

    mirar errore-2

--- src/app/libs/mysql.ts
+++ src/app/libs/mysql.ts
@@ -1,13 +1,20 @@
 import mysql from 'mysql2/promise';
-
+//conecta
 export async function connectToDatabase() {
   try {
-    const connection = await mysql.createConnection({
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT), // Asegúrate de convertir el puerto a número si es necesario
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+    const connection = await mysql.createConnection({      
+      // host: 'localhost',
+      // port: 3306,
+      // user: 'root',
+      // password: 'Basebodasabdra83',
+      // database: 'bodas'
+
+
+      host: 'instanciadbboda.c74u0c4wuzrm.eu-west-1.rds.amazonaws.com',
+      port: 3306,
+      user: 'admin',
+      password: 'oriol1983',
+      database: 'bodas'
     });
     console.log('Conexión exitosa a la base de datos');
     return connection;
@@ -16,3 +23,23 @@ export async function connectToDatabase() {
     return null;
   }
 }
+
+
+// import mysql from 'mysql2/promise';
+
+// export async function connectToDatabase() {
+//   try {
+//     const connection = await mysql.createConnection({
+//       host: process.env.DATABASE_HOST,
+//       port: Number(process.env.DATABASE_PORT), // Asegúrate de convertir el puerto a número si es necesario
+//       user: process.env.DATABASE_USER,
+//       password: process.env.DATABASE_PASSWORD,
+//       database: process.env.DATABASE_NAME,
+//     });
+//     console.log('Conexión exitosa a la base de datos');
+//     return connection;
+//   } catch (error) {
+//     console.error('Error al conectar a la base de datos:', error);
+//     return null;
+//   }
+// }
