3d9eb79aa5fbdd2ca775e6565bf1133bff203afd
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-04-04 20:06:07 +0200

    mirar errore-1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    poweredByHeader: false,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'www.sandraioriol.net',
+        },
+      ],
+    },
+    env: {
+      DATABASE_HOST: process.env.DATABASE_HOST,
+      DATABASE_PORT: process.env.DATABASE_PORT,
+      DATABASE_USER: process.env.DATABASE_USER,
+      DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+      DATABASE_NAME: process.env.DATABASE_NAME,
+    },
+    // Agrega aquí cualquier otra configuración específica de Next.js que necesites
+  };
+  
+  module.exports = nextConfig;
+  
