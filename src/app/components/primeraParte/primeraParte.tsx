ddc801fef2b8c0e7b958c86a6e88f9efe0261718
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-04-26 22:52:33 +0200

    plantitas

--- src/app/components/primeraParte/primeraParte.tsx
+++ src/app/components/primeraParte/primeraParte.tsx
@@ -43,26 +43,40 @@ const PrimeraParte: React.FC = () => {
 
     return (
         <div className={styles.container}>
+            <div className={styles.planta}>
+                        <Image src="/fondo-planta.png" alt="Planta de fondo" layout="fill" />
+                    </div>
+
+                    <div className={styles.plantaGeneral + ' ' + styles.plantaArribaDerecha}>
+                <Image src="/planta-3.png" alt="Planta 3 arriba derecha" layout="fill" />
+            </div>
+            <div className={styles.plantaGeneral + ' ' + styles.plantaCentroIzquierda}>
+                <Image src="/planta-2.png" alt="Planta 2 centro izquierda" layout="fill" />
+            </div>
+            <div className={styles.plantaGeneral + ' ' + styles.plantaAbajoIzquierda}>
+                <Image src="/planta-1.png" alt="Planta 1 abajo izquierda" layout="fill" />
+            </div>
             <div className={styles['texto-anuncio']} ref={textoAnuncioRef}>
-    <div className={styles['decoracion-izquierda']}>
-        <Image src="/inferior.png" alt="Decoración Izquierda"
-            priority
-            width={400}
-            height={206}
-        />
-    </div>
-        <div className={styles['texto-contenido']}> {/* Nuevo div para el texto con degradado */}
-        <h1 className={styles.tituloPrincipal}>NOS CASAMOS</h1>
-    <h2 className={styles.subtitulo}>Sandra & Oriol</h2>
-    {/* <p className={styles.invitacion}>Celebrad con nosotros<br />este momento inolvidable</p> */}
-        </div>
-        <div className={styles['decoracion-derecha']}>
-            <Image src="/superior.png" alt="Decoración Derecha"
-                priority
-                fill sizes="(max-width: 768px) 100px, 200px"
-            />
-        </div>
-    </div>
+                <div className={styles['decoracion-izquierda']}>
+                    <Image src="/inferior.png" alt="Decoración Izquierda"
+                        priority
+                        width={400}
+                        height={206}
+                    />
+                </div>
+                <div className={styles['texto-contenido']}>
+                    <h1 className={styles.tituloPrincipal}>NOS CASAMOS</h1>
+                    <h2 className={styles.subtitulo}>Sandra & Oriol</h2>
+                </div>
+                <div className={styles['decoracion-derecha']}>
+                    <Image src="/superior.png" alt="Decoración Derecha"
+                        priority
+                        fill sizes="(max-width: 768px) 100px, 200px"
+                    />
+                    {/* Añadir aquí la imagen de la planta con el estilo para animación */}
+                    
+                </div>
+            </div>
             <div className={styles['image-bw']}>
                 <Image src="/masia1.jpg" alt="Imagen en Blanco y Negro" fill priority />
             </div>
