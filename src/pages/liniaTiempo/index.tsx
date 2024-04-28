6620cc42faddd22928c21f670fc00c31a79211f9
Oriol-1 <68294321+Oriol-1@users.noreply.github.com>
2024-04-28 13:59:45 +0200

    datos finales-3

--- src/pages/liniaTiempo/index.tsx
+++ src/pages/liniaTiempo/index.tsx
@@ -6,9 +6,10 @@ import Image from 'next/image';
 const LiniDeTiempo = () => {
     return (
       <div>
-       <h1 className="text-center text-5xl font-sans my-5">
-      <b> ¡Vamos a celebrarlo!</b><br></br> Aquí el plan para nuestro gran día
-        </h1>
+       <h1 className="text-center font-sans my-5 text-xl sm:text-3xl md:text-4xl lg:text-5xl">
+  <b>¡Vamos a celebrarlo!</b><br /> Aquí el plan para nuestro gran día
+</h1>
+
          <Image src="/Logo5.svg" alt="Logo" width={100} height={100} style={{ width: '180px', height: 'auto',textAlign: 'center'
           , marginLeft: 'auto', marginRight: 'auto' }} />
         <TimelineBoda />
